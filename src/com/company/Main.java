c8ebce23a17302af7f850602ce4331e40aeb0d46
GraceTorresLeite <37751047+GraceTorresLeite@users.noreply.github.com>
2022-08-08 19:13:30 -0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// Desafio 2:
-        // Faça um programa em Java para ler um número que é um código de usuário.
-        // Caso este código seja diferente de um código armazenado internamente no algoritmo (igual a 1234)
-        // deve ser apresentada a mensagem ‘Usuário inválido!’.
-        // Caso o Código seja correto,deve ser lido outro valor que é a senha.
-        // Se esta senha estive incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’.
-        // Caso a senha esteja correta,deve ser mostrada a mensagem ‘Acesso permitido’
-
+	
         Scanner scanner = new Scanner(System.in);
 
         int code, password ;
